0b1693fef9fac47d56d9b8b548adb6f3ac04417d
irene iradukunda <iradukunda.irene200@gmail.com>
2023-05-11 01:03:06 +0200

    I did some minor changes, I removed some redundant codes in the UsersModel and UnitTestingModels.

--- NAVA_MVP1/Models/UsersModel.cs
+++ NAVA_MVP1/Models/UsersModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Reflection.Metadata.Ecma335;
 
 namespace NAVA_MVP1.Models
 {
@@ -31,86 +30,6 @@ namespace NAVA_MVP1.Models
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Both Password and Confirm password should match.")]
         public string? ConfirmPassword { get; set; }
-
-      
-        public UsersModel() 
-        {
-
-        }
-
-        public bool IsEmail(string useremail)
-        {
-             //Task1 - Testing if the user email is null: Passed successfully 
-            //Task2 - Testing the email domain validity: Not yet.
-            var flag = true;
-            if(useremail == null)
-                flag = false;
-            
-            return flag;
-        }
-        public bool IsConfirmedEmail(string confirmedemail)
-        {
-            //Task 1 - Testing if the confirmed eamil is not null: Passed successfully
-            //Task 2 - Testing the confiremd email domain validity: Not yet.
-            var flag = true;
-            if (confirmedemail == null)
-                flag = false;
-
-            return flag;
-        }
-        public bool IsUserName(string username)
-        {
-            //Task 1 - Testing if the username is not null: Passed successfully.
-            //Task 2 - Testing the username confirmity: Not yet.
-            var flag = true;
-            if (username == null)
-                flag = false;
-
-            return flag;
-        }
-        public bool IsPassword(string password)
-        {
-            //Task 1 - Testing if the password is not null: Passed successfully.
-            //Task 2 - Testing the password confirmity: Not yet.
-            var flag = true;
-
-            if (password == null)
-                flag = false;
-
-            return flag;
-        }
-        public bool IsConfirmedPassword(string confirmpassword)
-        {
-            //Task 1 - Testing if the confirmpassword is not null: Passed successfully.
-            //Task 2 - Testing the confirmpassword confirmity: Not yet.
-            var flag = true;
-
-            if (confirmpassword == null)
-                flag = false;
-
-            return flag;
-        }
-        public bool IsEmailSameAsConfirmedEmail(string useremil, string confirmemail)
-        {
-            //Task 1 - Testing if the email and confirm email are the same: Passed sucessfully.
-            var flag = true;
-
-            if (useremil != confirmemail)
-                flag = false;
-
-            return flag;
-
-        }
-        public bool IsPasswordSameAsConfirmedPassowrd(string password, string confirmpassword)
-        {
-            //Task 1 - Testing if the password and confirm password are the same: Passed sucessfully.
-            var flag = true;
-
-            if (password != confirmpassword)
-                flag = false;
-
-            return flag;
-        }
-
+       
     }
 }
--- TestNavaMVP1/UnitTestingModels.cs
+++ TestNavaMVP1/UnitTestingModels.cs
@@ -4,7 +4,7 @@ namespace TestNavaMVP1
     {
         UsersModel usermodel;
       /// <summary>
-      ///  This is the constructer of the Login class
+      ///  This is the constructer of the Register class
       /// </summary>
         public UnitTestingModels()
         {
@@ -19,86 +19,9 @@ namespace TestNavaMVP1
         }
 
         /// <summary>
-        /// This is where we are testing our methodes declared in the class login
+        /// This is where we are testing our methodes declared in the class 
         /// </summary>
 
-        [Fact]
-        public void TestingEmailValidity()
-        {
-            //Arrange
-            var UserEmail = "iradukunda@Finc.com";
-            //Act
-            var isemail = usermodel.IsEmail(UserEmail);
-            //Assert
-            Assert.True(isemail);
-        }
-
-        [Fact]
-        public void TestingConfirmedEmail()
-        {
-            //Arrange
-            var confirmedEmail = "iradukunda@Finc.com";
-            //Act
-            var isconfirmedemail = usermodel.IsConfirmedEmail(confirmedEmail);
-            //Assert
-            Assert.True(isconfirmedemail);
-        }
-
-        [Fact]
-        public void TestUsername ()
-        {
-            //Arrange
-            var username = "Irene";
-            //Act
-            var isusername = usermodel.IsUserName(username);
-            //Assert
-            Assert.True(isusername);
-        }
-
-        [Fact]
-        public void TestPassword()
-        {
-            //Arrange
-            var password = "Apples";
-            //Act
-            var ispassword = usermodel.IsPassword(password);
-            //Assert
-            Assert.True(ispassword);
-        }
-
-        [Fact]
-        public void TestConfirmPassword()
-        {
-            //Arrange
-            var confirmpassowrd = "Apples";
-            //Act
-            var isconfirmpassword = usermodel.IsConfirmedPassword(confirmpassowrd);
-            //Assert
-            Assert.True(isconfirmpassword); 
-        }
-
-        [Fact]
-        public void TestIsEmailSameAsConfirmedEmail()
-        {
-            //Arrange
-            var email = "Iradukunda@FINC.com";
-            var confirmemail = "Iradukunda@FINC.com";
-            //Act
-            var isemailsameasconfirmedemail = usermodel.IsEmailSameAsConfirmedEmail(email, confirmemail);
-            //Assert
-            Assert.True(isemailsameasconfirmedemail);
-        }
-
-        [Fact]
-        public void TestIsPasswordSameAsConfirmedPassword()
-        {
-            //Arrange
-            var password = "Apples";
-            var confirmpassword = "Apples";
-            //Act
-            var ispasswordsameasconfirmedpassword = usermodel.IsPasswordSameAsConfirmedPassowrd(password, confirmpassword);
-            //Assert
-            Assert.True(ispasswordsameasconfirmedpassword);
-        }
+       
     }
 }
